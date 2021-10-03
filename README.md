e19096ea0f408c58f5480b23b4e07c4626c96a21
aaron <aaronlim314@gmail.com>
2021-10-03 21:32:04 +1100

    current weather added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather Forecast
+Uses OpenWeather API
+
+# to do
+show forecast to users
+add icons for weather descriptions
+
--- script.js
+++ script.js
@@ -6,14 +6,6 @@ let latitude, longitude;
 
 getLocation();
 
-async function getWeather(){
-    alreadyCalled = true;
-    let urlString = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=3e5646a7f6d2244871efd9d9312f61f5';
-    let response = await fetch(urlString, {mode:'cors'});
-    weatherData = await response.json();
-    console.log(weatherData);
-    console.log(weatherData.clouds);
-}
 
 function getLocation(){
     if (navigator.geolocation) {
@@ -33,10 +25,38 @@ async function getWeatherForecast(lat,long){
     let response = await fetch(urlString, {mode:'cors'});
     weatherData = await response.json(); // await will block only the code within async func
     console.log(weatherData);
+    const dataObject = processWeatherData(weatherData);
+    renderWeatherData(dataObject);
 }
 
-function processWeatherData(){
-    
+function processWeatherData(data){
+    let daily = data.daily;
+    let hourly = data.hourly;
+    let current = data.current;
+    return{daily, hourly, current};
 }
 
+function renderWeatherData(dataObj){
+    console.log(dataObj);
+    renderDOM('current-title', 'Current Weather');
+    renderDOM('current-feels-like', 'Feels like: ' + Math.round(kToC(dataObj.current.feels_like)) + '°C');
+    renderDOM('current-temp', 'Temperature: ' + Math.round(kToC(dataObj.current.temp)) + '°C');
+    renderDOM('current-humidity', 'Humidity = ' + dataObj.current.humidity+ '%');
+    renderDOM('current-description', capitaliseFirst(dataObj.current.weather[0].description));
 
+}
+
+function kToC(kelvin){
+    return kelvin - 273.15;
+}
+
+function renderDOM(id, string){
+    const element = document.getElementById(id);
+    element.innerHTML = string;
+}
+
+function capitaliseFirst(string){
+    if(typeof string != 'string') return;
+    let output = string.charAt(0).toUpperCase() + string.slice(1);
+    return output;
+}
